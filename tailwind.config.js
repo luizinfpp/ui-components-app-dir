30e5d2aa613ee030463f7ae934004be0ef016155
luizinfpp <luizinfpp@gmail.com>
2022-11-15 21:53:38 -0300

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ["./app/**/*.{ts,tsx,jsx,js}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
